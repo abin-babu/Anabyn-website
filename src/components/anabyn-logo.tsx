120ed741ba00abe6983c83e3ebad4ad7e3322434
Abin Babu <sales@anabyn.com>
2025-10-15 18:01:36 +0000

    here is the logo in png format

--- src/components/anabyn-logo.tsx
+++ src/components/anabyn-logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export function AnabynLogo({ width, height, className }: { width: number; height: number; className?: string }) {
   return (
     <Image 
-      src="https://i.ibb.co/6gZ3sZn/logo.png" 
-      alt="Anabyn Global Ventures LLP Logo" 
+      src="https://i.ibb.co/VvfLz2h/image.png" 
+      alt="Anabyn Global Ventures LLP Shield Logo" 
       width={width} 
       height={height}
       className={className}
