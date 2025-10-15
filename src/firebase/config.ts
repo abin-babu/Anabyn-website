c7d4efa15b848e38fb39ae66eb16d536557cbfd0
Abin Babu <sales@anabyn.com>
2025-10-15 17:42:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9297553856-4efc0",
+  "appId": "1:909420400383:web:603af3f9e4d2dd4aeb46ca",
+  "apiKey": "AIzaSyAtMB0Pw_BrD-0wHPrn24YmcYI0YgHt9dY",
+  "authDomain": "studio-9297553856-4efc0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "909420400383"
+};
