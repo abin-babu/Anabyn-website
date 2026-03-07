ed4fae27b08f4263526120d74ff1be8809181c54
Anabyn Global Ventures LLP <sales@anabyn.com>
2026-03-07 16:03:23 +0000

    Set up a Firebase backend

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -2,30 +2,16 @@
 
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+import { initializeFirebase } from '@/firebase';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
 }
 
-// This function is for client-side initialization and should not be used on the server.
-function initializeClientFirebase() {
-    const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-    return {
-        firebaseApp: app,
-        auth: getAuth(app),
-        firestore: getFirestore(app)
-    };
-}
-
-
 export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
   const firebaseServices = useMemo(() => {
     // Initialize Firebase on the client side, once per component mount.
-    return initializeClientFirebase();
+    return initializeFirebase();
   }, []); // Empty dependency array ensures this runs only once on mount
 
   return (
