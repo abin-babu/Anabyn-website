d7aca3d011e30d688e50a0db80797802cd896143
Anabyn Global Ventures LLP <sales@anabyn.com>
2026-03-14 09:26:19 +0000

    Create two new files in the /public directory of this Firebase project:

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,1 @@
-import type { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin/',
-    },
-    sitemap: 'https://www.anabyn.com/sitemap.xml',
-  };
-}
+// This dynamic robots file is disabled in favor of static public/robots.txt to prevent build errors.
--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,5 +1,1 @@
-// This route is deprecated in favor of src/app/sitemap.ts
-// Next.js will automatically handle /sitemap.xml if sitemap.ts exists.
-export async function GET() {
-  return new Response(null, { status: 404 });
-}
+// This route is disabled in favor of static public/sitemap.xml to prevent build errors.
